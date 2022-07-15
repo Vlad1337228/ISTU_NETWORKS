1877320e744554a15fe38164e2a925107df47fc8
Vlad Kogevnikov <vlad.kogevnikov.com@gmail.com>
2022-07-15 14:03:52 +0400

    first

---
+++ client_l3/client_l3/client_l3.cpp
@@ -0,0 +1,164 @@
+﻿#pragma comment(lib,"ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <signal.h>
+#include <string>
+#include <signal.h>
+#include <ws2tcpip.h>
+#define INADDR_BROADCAST  
+
+using namespace std;
+#pragma warning(disable: 4996)
+SOCKET connection;
+bool endGameFlag = false;
+void MessHandler()
+{
+	int mess_length;
+	while (true)
+	{
+		if (recv(connection, (char*)&mess_length, sizeof(int), NULL) > 0)
+		{
+			char* array_clients = new char[mess_length + 1];
+			array_clients[mess_length] = '\0';
+			if (recv(connection, array_clients, mess_length, NULL) >0)
+			{
+				cout << array_clients << endl;
+			}
+			else
+			{
+				cout<<"The End" << endl;
+				endGameFlag = true;
+				break;
+			}
+			//cout << array_clients << endl;
+			delete[] array_clients;
+		}
+		else
+		{
+			endGameFlag = true;
+			cout << "The End" << endl;
+			break;
+		}
+	}
+}
+
+
+void Bad_Key_Ctrl_C(int sig) 
+{
+	signal(sig, Bad_Key_Ctrl_C); 
+}
+
+int iResult;
+
+
+
+void GetAllIpAddressServers() 
+{
+
+	char buf[1024];
+	ZeroMemory(buf, sizeof(buf));
+	SOCKADDR_IN addr_in;
+	int sizeAddrServers = sizeof(addr_in);
+	addr_in.sin_port = htons(1111);
+	addr_in.sin_family = AF_INET;
+	addr_in.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+	SOCKET socketU = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (socketU == INVALID_SOCKET) 
+	{
+		printf("Error %ld\n", WSAGetLastError());
+	}
+	char opt = 5;
+	setsockopt(socketU, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt));
+	socketU;
+	
+	char mes[256] = "1111";
+	if (sendto(socketU, mes, sizeof(mes), NULL, (sockaddr*)&addr_in, sizeAddrServers) == SOCKET_ERROR)
+		cout << "Haven't message " + WSAGetLastError() << endl;
+	else cout << "Search servers" << endl;
+
+	
+	int repeat = 1;
+	string ipServer;
+	while (repeat != 4)
+	{
+		repeat++;
+		DWORD opt = 2000;
+		setsockopt(socketU, SOL_SOCKET, SO_RCVTIMEO, (const char*)&opt, sizeof DWORD);
+		iResult = recvfrom(socketU, buf, sizeof(buf), NULL, (sockaddr*)&addr_in, &sizeAddrServers);
+		if (iResult < 0)
+		{
+			break;
+		}
+		char clientIp[256];
+		ZeroMemory(clientIp, 256);
+		inet_ntop(AF_INET, &addr_in.sin_addr, clientIp, 256);
+
+		ipServer =  clientIp;
+		cout << "Found ip server" + ipServer << endl;
+	}
+	closesocket(socketU);
+	cout << "Socket closed \n" << endl;
+}
+
+int main(int args, char* argv[])
+{
+	void (*isBadKeys)(int);
+	isBadKeys = signal(SIGINT, Bad_Key_Ctrl_C);
+	SetConsoleCtrlHandler(NULL, true); 
+	
+	WSADATA wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0)
+	{
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	GetAllIpAddressServers();
+	char* ip = new char[16];
+	cin >> ip;
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr(ip);
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+	
+	connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(connection, (SOCKADDR*)&addr, sizeof(addr)) != 0)
+	{
+		cout << "Error \n";
+		return 1;
+	}
+	cout << "Connected \n";
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MessHandler, NULL, NULL, NULL);
+	string message;
+	while (true)
+	{
+		getline(cin,message);
+		int length_without_date = message.size();
+		if (message.length() > 1 && !endGameFlag)
+		{
+			cout << "Is not char. Pls write one char" << endl;
+			continue;
+		}
+		if (message[0] == '0' && message.length() == 1) 
+		{
+			GetAllIpAddressServers();
+			continue;
+		}
+		if (cin.fail() || cin.eof() || length_without_date <= 0)
+		{
+			cin.clear();
+			continue;
+		}
+		send(connection, (char*)&length_without_date, sizeof(int), NULL);
+		send(connection, message.c_str(), length_without_date, NULL);
+		signal(SIGINT, isBadKeys);
+		message.clear();
+	}
+	system("pause");
+	return 0;
+}
---
+++ seti_l3/seti_l3/seti_l3.cpp
@@ -0,0 +1,211 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <string>
+#include <signal.h>
+#include <conio.h>
+#include <string> 
+#include <sstream>
+#pragma comment(lib,"ws2_32.lib") 
+#pragma warning(disable: 4996)
+using namespace std;
+
+
+SOCKET Connections[100]; // массив подключений
+int Counter = 0; // нужен для кол-ва подключенных клиентов
+int maxCountClient=3; // кол-во максимального размера подключенных клиентов
+int currentGamer = 0;
+string hiddenLetter;
+
+//CRITICAL_SECTION  cs;
+
+void Bad_Key_Ctrl_C(int but) {
+
+	signal(but, Bad_Key_Ctrl_C); 
+}
+
+
+void ReceivingBroadcastAddressHendler(SOCKET udpSocket) {
+	char buf[256];
+	sockaddr_in sockADDR;
+	int lengthMes = sizeof(sockADDR);
+	ZeroMemory(&sockADDR, lengthMes);
+	ZeroMemory(&buf, sizeof(buf));
+
+	while (true) 
+	{
+		if (recvfrom(udpSocket, (char*)&buf, sizeof(buf), NULL, (sockaddr*)&sockADDR, &lengthMes) == SOCKET_ERROR)
+			cout << "Haven't message " + WSAGetLastError() << endl;
+		if (sendto(udpSocket, (char*)&buf, sizeof(buf), NULL,(sockaddr*)&sockADDR, lengthMes) == SOCKET_ERROR)
+			cout <<  "Error: Message don't send " + WSAGetLastError() << endl;
+	}
+	closesocket(udpSocket);
+}
+
+void giveAwayMyIpAddress() {
+	
+	SOCKADDR_IN addr;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+	int sizeAddr = sizeof(addr);
+
+	SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == INVALID_SOCKET) 
+	{
+		printf("Error \n", WSAGetLastError());
+		return;
+	}
+
+	if (bind(sock, (SOCKADDR*)&addr, sizeAddr) == SOCKET_ERROR) 
+	{
+		cout << "Error \n";
+		closesocket(sock);
+		return;
+	}
+
+	char optval = 5;
+	setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &optval, sizeof(optval));
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ReceivingBroadcastAddressHendler,(LPVOID)(sock), NULL, NULL);
+
+}
+
+
+
+
+void SendMess(int index)
+{
+	int length_message;
+	int countWin;
+	//EnterCriticalSection(&cs);
+	while (true)
+	{
+		recv(Connections[index], (char*)&length_message, sizeof(int), NULL);
+		char* msg = new char[length_message + 1];
+		msg[length_message] = '\0';
+		recv(Connections[index], msg, length_message, NULL);
+
+		if (currentGamer == index)
+		{
+			for (int i = 0; i < Counter; i++)
+			{
+				const char* mes = new char[256];
+				int len;
+				if (hiddenLetter == msg)
+				{
+					bool flag = false;
+					if (index == i)
+					{
+						len = 10;
+						mes = " You win";
+						send(Connections[i], (char*)&len, sizeof(int), NULL);
+						send(Connections[i], mes, len, NULL);
+						closesocket(Connections[i]);
+						flag = true;
+					}
+					else
+					{
+						len = 10;
+						mes = "You lose";
+						send(Connections[i], (char*)&len, sizeof(int), NULL);
+						send(Connections[i], mes, len, NULL);
+						closesocket(Connections[i]);
+					}
+					if (flag && i==(Counter-1))
+					{
+						return;
+					}
+
+				}
+				else
+				{
+					if (index == i)
+					{
+						len = 10;
+						mes = "No";
+						send(Connections[i], (char*)&len, sizeof(int), NULL);
+						send(Connections[i], mes, len, NULL);
+					}
+				}
+			}
+			currentGamer+=1;
+			if (currentGamer >= Counter)
+			{
+				currentGamer = 0;
+			}
+		}
+		else
+		{
+			const char* mes = new char[256];
+			int len=15;
+			mes = "Not your tern";
+			send(Connections[index], (char*)&len, sizeof(int), NULL);
+			send(Connections[index], mes, len, NULL);
+		}
+		delete[] msg;
+	}
+	//LeaveCriticalSection(&cs);
+}
+
+int RandomNumberRange(int a, int b)
+{
+	srand(time(NULL));
+	int d= rand() % (b - a + 1) + a;
+	return d;
+}
+
+int main(int args, char* argv[])
+{
+	char n= RandomNumberRange(65, 90);
+	hiddenLetter = n;
+
+	char* ip = new char[16];
+	cin >> ip;
+
+	WSADATA wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0)
+	{
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	giveAwayMyIpAddress();
+
+	SOCKADDR_IN addr;
+	addr.sin_addr.s_addr = inet_addr(ip);
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+	int sizeofaddr = sizeof(addr);
+
+	SOCKET sock = socket(AF_INET,SOCK_STREAM, NULL);
+	
+
+
+	bind(sock,(SOCKADDR*)&addr,sizeof(addr));
+	listen(sock, maxCountClient);
+
+	SOCKET newConnection;
+	for (int i = 0; i < 100; i++)
+	{
+		newConnection = accept(sock, (SOCKADDR*)&addr, &sizeofaddr);
+		if (newConnection == 0)
+		{
+			cout << "Error \n";
+			return 1;
+		}
+		else
+		{
+			cout << "Connected successful \n";
+		}
+		Connections[i] = newConnection;
+		Counter++;
+		string message = "Hidden letter " + hiddenLetter;
+		send(newConnection, (char*)&message, sizeof(message), NULL);
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SendMess, (LPVOID)(i), NULL, NULL);
+	}
+	system("pause");
+	return 0;
+}
+
+
+
